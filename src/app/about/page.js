90c1416169c7124903e032e60db26fb201ae448d
iamsanjayp <itsanjayp@gmail.com>
2024-10-26 10:05:31 +0530

    created first next js webapp.

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <div>
+      <h1>This is page 2</h1>
+      <nav>
+        <Link href="/">Go to home page</Link>
+      </nav>
+    </div>
+  );
+}
